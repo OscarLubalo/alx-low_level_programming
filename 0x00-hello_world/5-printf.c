70175000e6a28d3235a5f5d7ea9caa9cb7502b7b
oscarlubalo <l.otienooscar@gmail.com>
2023-06-15 14:25:45 -0700

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 
 /**
@@ -13,9 +14,8 @@
 int main(void)
 
 {
+ printf("with proper grammar, but the outcome is a piece of art,\n");
 
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-        return (0);
+ return (0);
 
 }
