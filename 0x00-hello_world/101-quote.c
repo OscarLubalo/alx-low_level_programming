b2102f39879bbeb23b517d38cf5e3b3ed4931f25
oscarlubalo <l.otienooscar@gmail.com>
2023-06-19 13:26:58 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 /**
 * main - Entry point
 *
 * Return: Always 1 (Success)
 */
 int main(void)
-{	printf("and that piece of art is usefull\ -Dora Karpar, 2015-10-19\n", 59);
-	return (0);
+{
+	const char *text;
+
+	text = "and that piece of art is useful\" - Dora Korpar,2015-10-19\n";
+	write(2, text, strlen(text));
+
+	return (1);
 }
