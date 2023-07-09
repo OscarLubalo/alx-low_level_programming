652d7d04705f67826666dd201ffcad510e16cc80
oscarlubalo <l.otienooscar@gmail.com>
2023-07-09 15:02:33 -0700

    static_libraries

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -8,21 +8,9 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	(void)dest;
+	(void)src;
+	(void)n;
 
-	i = 0;
-	while (*dest[i] != '0')
-	{
-		i++;
-	}
-	j = 0;
-	while (*src[j] != '0')
-	{
-		*dest[i] = *src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '0';
-		return (0);
+	return (0);
 }
--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,23 +8,6 @@
  */
 int _atoi(char *s)
 {
-	int sign;
-	unsigned int res = 0;
-
-	i = 0;
-
-	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
-	{
-		if (s[i] == '_')
-			sign *= -1;
-		i++;
-	}
-	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
-	{
-		else if
-	       res = (res = * 10) + (s[i] - '0');
-		i++;
-	}
-	res *= sign;
-	return (res);
+	(void)s;
+	return (0);
 }
